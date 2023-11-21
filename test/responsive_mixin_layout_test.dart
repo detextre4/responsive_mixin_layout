ed438a8d92a1df715473c99c3f492ab0f9039d7f
detextre4 <detextre4@gmail.com>
2023-11-21 13:44:42 -0400

    changed files

--- test/responsive_mixin_layout_test.dart
+++ test/responsive_mixin_layout_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:responsive_mixin_layout/flutter_responsive_layout.dart';
+import 'package:responsive_mixin_layout/responsive_mixin_layout.dart';
 
 void main() {
   testWidgets('Mobile layout should display correctly',
